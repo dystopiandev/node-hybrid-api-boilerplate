6ad88b0bc72810730ebd95c6dc412033b1f6e1b1
r3dh4r7 <redhart@dualsight.org>
2019-05-30 13:28:44 +0100

    Fix seeder.

--- db/seeders/20190401024658-demo-user.js
+++ db/seeders/20190401024658-demo-user.js
@@ -1,19 +1,22 @@
 'use strict'
 
+const config = require('../../config')
+const bcrypt = require('bcrypt')
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
       {
         firstName: 'John',
         lastName: 'Doe',
-        email: 'user@domain.tld',
-        password: '?'
+        email: 'user@domain.com',
+        password: bcrypt.hashSync('password', config.server.saltRounds)
       },
       {
         firstName: 'Eva',
         lastName: 'Foster',
-        email: 'user2@domain.tld',
-        password: '?'
+        email: 'user2@domain.com',
+        password: bcrypt.hashSync('password', config.server.saltRounds)
       }
     ], {})
   },
